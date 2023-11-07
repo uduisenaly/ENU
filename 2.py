0ec39987b84c707107bb13f0543dd98a226d6522
uduisenaly <147018801+uduisenaly@users.noreply.github.com>
2023-11-07 22:31:13 +0600

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-import xgboost as xgb
+from sklearn.ensemble import AdaBoostClassifier
 
 # Загрузка данных
 data = pd.read_csv('airport_data.csv')
@@ -13,8 +13,8 @@ y = data['is_north']
 # Разделение данных
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Создание и обучение модели XGBoost
-model = xgb.XGBClassifier(n_estimators=100, max_depth=3)
+# Создание и обучение модели AdaBoost
+model = AdaBoostClassifier(n_estimators=50)
 model.fit(X_train, y_train)
 
 # Оценка модели
